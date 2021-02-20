e174f34d6a485c77e1ab2d03db25f167b345da25
Margus Lehiste <margus@createcore.com>
2021-02-20 19:09:39 +0200

    oldest first

--- src/pages/index.js
+++ src/pages/index.js
@@ -45,7 +45,7 @@ export default function Home({ data }) {
 
 export const query = graphql`
   query {
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: ASC }) {
       totalCount
       edges {
         node {
